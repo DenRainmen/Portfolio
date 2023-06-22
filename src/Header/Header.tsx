89b5e8c836940d81bdc3f8da580c19fa65c241f9
Den <potarykindenis@gmail.com>
2023-06-22 16:45:20 +0300

    Добавил адаптив для дополнительного меню

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import s from "./Header.module.scss";
-import { Nav } from "../Nav/Nav";
-/* import c from "../common/styles/Container.module.css"; */
+
 import "../App.css"
 
+import  {useState}  from "react"
+
 export const Header = () => {
+
+
+const[active, setActive] = useState(false)
+
+const onClickHandler = () => {
+ /*  alert("YO") */
+ setActive(!active)
+
+}
+
+
   return (
     <header className={`container ${s.header}`}>
       <div className={`${s.header}`}>
-        <Nav />
+
+
+
+        <div className={s.photo}></div>
+
+        <div
+         className={active ? `${s.burger} ${s.active}` : s.burger}
+         onClick = {onClickHandler}
+         >
+
+          <span></span>
+
+        </div>
+
+        <nav className={active ? `${s.nav} ${s.active}` : s.nav}>
+          <a href="#">Home</a>
+          <a href="#">Skills</a>
+          <a href="#">Projects</a>
+          <a href="#">Contacts</a>
+        </nav>
+
       </div>
     </header>
   );
