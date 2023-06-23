ec231e832c24216986d2a23cbcbf8d75def33846
Den <potarykindenis@gmail.com>
2023-06-23 16:58:46 +0300

    Добавил описания скиллов

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -21,16 +21,81 @@ export const Skills = () => {
           skillIcon={htmlIcon}
         />
         <SkillCard
-         skillTitle={"CSS"}
-         skillDecription={"Decription for CSS"}
-         skillIcon={cssIcon}
-          />
+          skillTitle={"CSS"}
+          skillDecription={"Decription for CSS"}
+          skillIcon={cssIcon}
+        />
 
         <SkillCard
           skillTitle={"JavaScript"}
           skillDecription={"Decription for JavaScript"}
           skillIcon={jsIcon}
         />
+      </div>
+      <div className={s.skillsDescription}>
+        <ul>
+          <li>
+            Знание JavaScript ES16-22;
+          </li>
+          <li>
+            Умение верстать по технике PixelPerfect;
+          </li>
+          <li>
+            Нейминг по методологии БЭМ;
+
+          </li>
+          <li>
+            Работа с HTML-препроцессорами (pug);
+
+          </li>
+          <li>
+            Работа с CSS-препроцессорами (SCSS/SASS);
+
+          </li>
+          <li>
+            Адаптивная/кроссбраузерная верстка;
+
+          </li>
+          <li>
+            Работа с  axios, Fetch API;
+
+
+          </li>
+          <li>
+            Работа с инструментами сборки (npm, yarn, Vite, CRA);
+
+
+          </li>
+          <li>
+            Компонентный подход в разработке;
+          </li>
+          <li>
+            Умение работать с чужим кодом;
+          </li>
+          <li>
+            Соблюдение заданных на проекте стандартов и требований;
+          </li>
+          <li>
+            Базовое владение JS-библиотекой React + Redux;
+          </li>
+          <li>
+            Знание систем контроля версий Git;
+          </li>
+          <li>
+            Базовые навыки работы с инструментами дизайна и обработки изображений(Figma);
+
+          </li>
+          <li>
+            Опыт написания тестов;
+          </li>
+          <li>
+            TypeScript.
+          </li>
+
+
+
+
+        </ul>
       </div>
     </section>
   );
